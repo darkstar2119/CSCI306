31a15b37fcbfb877a06a551c32f94fb1b4fd6553
Kevyn Young <keyoung@mines.edu>
2012-10-02 09:39:29 -0600

    finished JUnitTest for Envelope

--- JUnitProject/TestEnvelope.java
+++ JUnitProject/TestEnvelope.java
@@ -2,16 +2,35 @@ package test;
 
 import junit.framework.Assert;
 import org.junit.*;
-import price.Envelope;
+
+import source.Envelope;
 
 public class TestEnvelope {
 
-	Envelope testE = new Envelope(0,0,0,0,0);
+	Envelope testE = new Envelope(5,3,2,1,6);
 	
 	@Test
 	public void testTotalCents() {
-		int expected = 0;
+		int expected = 606;
 		int actual = testE.getTotalCents();
 		Assert.assertEquals(expected,actual);
+	}
+	
+	Envelope testE2 = new Envelope(7,1,5,11,9);
+	
+	@Test
+	public void testTotalCents2() {
+		int expected = 839;
+		int actual = testE2.getTotalCents();
+		Assert.assertEquals(expected, actual);
+	}
+	
+	Envelope testE3 = new Envelope(6,2,4,7,1);
+	
+	@Test
+	public void testTotalCentrs3() {
+		int expected = 726;
+		int actual = testE3.getTotalCents();
+		Assert.assertEquals(expected, actual);
 	}
 }
