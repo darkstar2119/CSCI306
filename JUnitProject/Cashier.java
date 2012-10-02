43779be0fdc553f134520718057117d345cfba3f
Nate Caroe <ncaroe@mines.edu>
2012-10-02 09:35:50 -0600

    Finished Cashier.java

--- JUnitProject/Cashier.java
+++ JUnitProject/Cashier.java
@@ -4,30 +4,30 @@ public class Cashier {
 	public Envelope makeChange(Envelope e, int dollars, int cents) {
 		
 		int cmpChange = (dollars*100) + cents;
-		
-		
-		/*int remCents = cents;
-		int q = remCents / 25;
-		remCents = remCents - (q*25);
-		int d = remCents / 10;
-		remCents = remCents - (d*10);
-		int n = remCents / 5;
-		remCents = remCents - (n*5);
-		int p = remCents;*/
-		
 		int chng = e.getTotalCents() - cmpChange;
-	
 		
-		String error = "Negative balance";
-		if (cmpChange < 0) {
+		String error = "Negative balance.";
+		if (chng < 0) {
 			try {
 				throw new NegativeBalanceException(error);
-			} catch (NegativeBalanceException e1) {
+			} catch (NegativeBalanceException nbe) {
+				System.out.println(error);
 			}
 		}
 			
-		return e;
+		int remChng = chng;
+		int dollas = remChng / 100;
+		remChng = remChng - (dollas*100);
+		int q = remChng / 25;
+		remChng = remChng - (q*25);
+		int d = remChng / 10;
+		remChng = remChng - (d*10);
+		int n = remChng / 5;
+		remChng = remChng - (n*5);
+		int p = remChng;
 		
+		Envelope chngEnvelope = new Envelope(dollas,q,d,n,p);
+		return chngEnvelope;
 		
 	}
 }
